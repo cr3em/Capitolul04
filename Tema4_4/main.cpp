1a899108f34e81cbf15be08796ecdf9cd4c33f57
cr3em <emil_cretan@yahoo.com>
2019-10-29 22:59:53 +0200

    Pointeri spre heap

--- Tema4_4/main.cpp
+++ Tema4_4/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+/* Pointeri pe heap */
+
+double *pnt = new double(1.3);
+
+int main()
 {
-    cout << "Hello World!" << endl;
+    double *pnt1 = new double(2.3);
+    double *pnt2 = new double(5.4);
+    double *pntMedie = new double;
+    *pntMedie = (*pnt + *pnt1 + *pnt2)/3;
+    cout << "Media aritmetica este " << *pntMedie << endl;
+
+    delete pnt; pnt = nullptr;
+    delete pnt1; pnt1 = nullptr;
+    delete pnt2; pnt2 = nullptr;
+
+
     return 0;
 }
