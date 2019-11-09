203ec82ced4669dabc0593aefabad0aed37a92e4
cr3em <emil_cretan@yahoo.com>
2019-11-10 01:26:30 +0200

    Siruri si siruri
    completat tema - numararea cuvintelor din sir

--- Tema4_6/main.cpp
+++ Tema4_6/main.cpp
@@ -4,16 +4,24 @@ using namespace std;
 
 int main()
 {
-    char sirStack[] = "Aici poti scrie ce vrei si cat vrei de mult!";  // alocare sir pe stack
+    char sirStack[] = "Aici poti sa scri ceea ce vrei!";  // alocare sir pe stack
     char *sirHeap = new char{sizeof(sirStack)}; // alocare sir pe heap
 
     for(unsigned int i = 0; i < sizeof(sirStack); ++i){  // copiaza continut de pe stack pe heap
         sirHeap[i] = sirStack[i];
     }
     cout << sirHeap << endl;
+
+    int contor = 1;
+    for(unsigned int i = 0; i < sizeof(sirStack); ++i){
+        if (sirStack[i] == ' ')
+            contor += 1;
+    }
+    cout << "Numar cuvinte in sir: " << contor << endl;  // afisare numar de cuvinte din sir
+
     cout << "Sirul de pe stack are " << sizeof(sirStack) << " caractere\n";  // afisare numar de caractere din sir
 
-    delete sirHeap; sirHeap = nullptr;
+    delete[] sirHeap; sirHeap = nullptr;
 
-return 0;
+    return 0;
 }
